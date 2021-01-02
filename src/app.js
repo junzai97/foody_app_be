5ba7c3ffa82d7b46ffa1f888a8c59e1a889d4927
Ka Chun <34387770+kachun333@users.noreply.github.com>
2021-01-02 14:24:47 +0800

    Update app.js
    use MeatRouter

--- src/app.js
+++ src/app.js
@@ -2,13 +2,14 @@ const express = require('express');
 require('./config/mysql');
 const indexRouter = require('./routers/index.router');
 const userRouter = require('./routers/users.router');
+const meatRouter = require('./routers/meats.router');
 
 const app = express();
-
 
 //Add in the routes path under here
 app.use(express.json());
 app.use(indexRouter);
 app.use(userRouter);
+app.use(meatRouter);
 
 module.exports = app;
