bcfb50220208b7471a8c60c10479ca02c966339b
JKQuah <kobekai1997@gmail.com>
2021-01-02 19:28:25 +0800

    add readPost, updatePost, and deletePost methods

--- src/repository/posts.repository.js
+++ src/repository/posts.repository.js
@@ -6,9 +6,6 @@ const { InvalidEntity } = require('../exceptions/InvalidEntity.exception');
 const { createPlaceholderString } = require('../utils/mysql.utils');
 
 function createPost(post){
-    if(!post instanceof Post){
-        throw new InvalidEntity("This is not a post object");
-    }
     return new Promise((resolve, reject)=>{
         connection.query(
             `INSERT INTO post VALUES (${createPlaceholderString(9)})`,
@@ -30,25 +27,52 @@ function createPost(post){
     });
 }
 
-function readPost(){
+function readPosts(user_id){
     return new Promise ((resolve, reject) => {
-        connection.query('SELECT * FROM posts WHERE user_id = ?', [username], (error, results, fields) => {
+        connection.query('SELECT * FROM post WHERE user_id = ?', [user_id], (error, results, fields) => {
             error? reject(error):resolve(results);
         }) 
     }) 
 }
 
-function updatePost(){
 
+function readPost(post, user_id){
+    return new Promise ((resolve, reject) => {
+        connection.query('SELECT * FROM post WHERE user_id = ? WHERE id = ?', [user_id, post.id], (error, results, fields) => {
+            error? reject(error):resolve(results);
+        }) 
+    }) 
 }
 
-function deletePost(){
+function updatePost(post){
+    var now = new Date().toISOString().slice(0, 19).replace('T', ' ');
+   
+    return new Promise((resolve, reject) => {
+        connection.query(
+            `UPDATE post ` +
+            `SET description = ?, services = ?, cleanliness = ?, taste = ?, price = ?, last_modified_date = ? ` + 
+            `WHERE id = ?;`,
+        [post.description, post.services, post.cleanliness, post.taste, post.price, now, post.id],
+        (error, results, fields) => { 
+            error ? reject(error) : resolve(results);
+        })
+    })
+}
 
+function deletePost(post){
+    return new Promise((resolve, reject)=>{
+        connection.query(`DELETE FROM post WHERE id=?;` , 
+        [post.id],
+        (error, results, fields) => {
+            error ? reject(error) : resolve(results);
+        })
+    })
 }
 
 module.exports = {
     createPost,
     readPost,
+    readPosts,
     updatePost,
     deletePost
 }
--- src/routers/posts.router.js
+++ src/routers/posts.router.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const Post = require('../entities/post.entity');
-const { createPost } = require('../repository/posts.repository');
+const { createPost, readPosts, updatePost, deletePost } = require('../repository/posts.repository');
 const PostDTO = require('../dtos/postDTO.dto');
+const { route } = require('./index.router');
 const router = express.Router();
 
 //CREATE YOUR REST API UNDER HERE
+// Create A Post
 router.post('/post', async (req, res) => {
     const postDTO = req.body;
-    if(!postDTO instanceof PostDTO){
-        res.status(400).send("invalid request body");
-    }
-
     try {
         var post = new Post(
             null,
@@ -23,6 +21,59 @@ router.post('/post', async (req, res) => {
         );
         const result = await createPost(post);
         res.status(200).send(`Post with id ${result.insertId} had saved successfully`);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
+})
+
+// Read A Post
+router.get('/post', async (req, res) => {
+    const postDTO = req.body;
+    try {
+        var post = await readPosts(postDTO.user_id);
+        res.status(200).send(post);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
+})
+
+// Update A Post
+router.put('/post', async (req, res) => {
+    const postDTO = req.body;
+    try {
+        var post = new Post(
+            postDTO.id,
+            postDTO.user_id, 
+            postDTO.description,
+            postDTO.services,
+            postDTO.cleanliness,
+            postDTO.taste,
+            postDTO.price,
+        );
+        const result = await updatePost(post);
+        
+        res.status(200).send('Post had updated successfully.');
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
+})
+
+// Delete A Post
+router.delete('/post', async (req, res) => {
+    const postDTO = req.body;
+    try {
+        await deletePost(postDTO);
+        var result = `{
+            "status" : 200,
+            "respond" : "success",
+            "mssg" : "Post had deleted successfully"
+        }`;
+        res.status(200).send(JSON.parse(result));
 
     } catch (error) {
         console.log(error);
