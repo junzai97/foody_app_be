8bb704ef37241e3fa2b345a6418d82b98b5f3af7
Clement Saw Kiat Chun <cskiat06@gmail.com>
2021-01-06 14:01:32 +0800

    WIP updating user module

--- src/repository/users.repostitory.js
+++ src/repository/users.repostitory.js
@@ -1,15 +1,48 @@
-const { createPoolCluster } = require('mysql');
 const { connection } = require('../config/mysql');
-const { auth,db } = require('../config/firebase');
+const {createPlaceholderString} = require('../utils/mysql.utils')
 
-function getUser(username){
+function createUser(user){
+    return new Promise((resolve, reject)=> {
+        connection.query(
+            `INSERT INTO USER (
+                ID,
+                FIREBASE_UID,
+                USERNAME,
+                EMAIL,
+                PASSWORD,
+                GENDER,
+                BIOGRAPHY,
+                CREATED_DATE,
+                LAST_MODIFIED_DATE
+            ) VALUES (${createPlaceholderString(9)})`,
+            [
+                user.id,
+                user.imageStorageId,
+                user.username,
+                user.email,
+                user.password,
+                user.gender,
+                user.biography,
+                user.createdDate,
+                user.lastModifiedDate
+            ],
+            (error, results, fields) => {
+                error? reject(error) : resolve(results);
+            }
+        );
+    });
+}
+
+function getUser(email){
     return new Promise ((resolve, reject) => {
-        connection.query('SELECT * FROM user WHERE username = ?', [username], (error, results, fields) => {
-            error? reject(error):resolve(results);
+        connection.query('SELECT * FROM user WHERE email = ?', [email], (error, results, fields) => {
+            user = results[0];
+            error? reject(error):resolve(user);
         }) 
     }) 
 }
 
 module.exports = {
-    getUser
+    getUser,
+    createUser
 }
--- src/routers/users.router.js
+++ src/routers/users.router.js
@@ -1,17 +1,66 @@
 const express = require('express');
 const router = express.Router();
-const {getUser} = require('../repository/users.repostitory')
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt')
+const { toMysqlTimestampString } = require('../utils/mysql.utils');
+const { createUser, getUser} = require('../repository/users.repostitory')
+const { createStorage } = require('../repository/storage.repostitory');
+const User = require('../entities/user.entity');
+const { auth } = require('../config/firebase');
+const AttachmentType = require('../enums/attachmentType.enum');
+
+router.post('/users', async (req,res) => {
+    const userDTO = req.body;
+    try {
+        const savedStorageResult = await createStorage(
+            userDTO.base64String,
+            AttachmentType.USER
+        );
+        const user = new User(
+            null,
+            savedStorageResult.insertId,
+            userDTO.username,
+            userDTO.email,
+            userDTO.password,
+            userDTO.gender,
+            userDTO.biography,
+            toMysqlTimestampString(new Date()),
+            toMysqlTimestampString(new Date()),
+        );
+        const salt = await bcrypt.genSalt(5);
+        user.password = await bcrypt.hash(user.password, salt);
+        const savedResult = await createUser(user);
+        res
+        .status(200)
+        .send(`User with ${savedResult.insertId} saved succesfully`);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send(err);
+    }
+})
 
 
-//CREATE YOUR REST API UNDER HERE
-router.get('/users', async (req,res) => {
+router.post('/users/login', async (req,res) => {
+    const userDTO = req.body;
     try{
-        let results = await getUser(req.body.username);
-        res.status(200).send(results);
-    }catch (err){
-        console.log(err)
+        const user = await getUser(userDTO.email);
+
+        if(!user){
+            return res.status(400).send("Your email or password is incorrect.")
+        }
+        
+        const isMatch = await bcrypt.compare(userDTO.password, user.password);
+        
+        if(!isMatch){
+            return res.status(400).send("Your email or password is incorrect.")
+        }
+
+        res.status(200).send(user);
+
+    } catch(err){
+        console.log(err);
         res.status(400).send(err);
-    }   
+    }
 })
 
 module.exports = router;
