2b4fb8a1f234db7ed9f638d190f5da15b92e7ec7
Ka Chun <34387770+kachun333@users.noreply.github.com>
2021-01-06 12:11:37 +0800

    type checking at meats.repository.js

--- src/repository/meats.repostitory.js
+++ src/repository/meats.repostitory.js
@@ -1,7 +1,12 @@
 const { connection } = require("../config/mysql");
-const {createPlaceholderString} = require('../utils/mysql.utils')
+const { createPlaceholderString } = require("../utils/mysql.utils");
+const { hasMissingKey } = require("../utils/compare.utils");
+const Meat = require("../entities/meat.entity");
 
 function createMeat(meat) {
+  if (hasMissingKey(meat, new Meat())) {
+    throw new Error("Cannot save invalid Meat object to DB");
+  }
   return new Promise((resolve, reject) => {
     connection.query(
       `INSERT INTO MEAT (
