83277e20755a94ea8c7a411894cdf5bc1a2d2f2b
Ka Chun <34387770+kachun333@users.noreply.github.com>
2021-01-07 13:05:48 +0800

    fix typo

--- src/repository/meatUsers.repostitory.js
+++ src/repository/meatUsers.repostitory.js
@@ -7,7 +7,11 @@ const MeatUser = require("../entities/meatUser.entity");
 const MeatUserStatus = require("../enums/meatUserStatus.enum");
 const MeatUserRole = require("../enums/meatUserRole.enum");
 
-function createMeatOrganiser(meatId, userId) {
+function createMeatUser(
+  meatId,
+  userId,
+  meatUserRole = MeatUserRole.PARTICIPANT
+) {
   return new Promise((resolve, reject) => {
     connection.query(
       `INSERT INTO MEAT_USER (
@@ -23,35 +27,7 @@ function createMeatOrganiser(meatId, userId) {
         null,
         meatId,
         userId,
-        MeatUserRole.ORGANISER,
-        MeatUserStatus.GOING,
-        toMysqlTimestampString(new Date()),
-        toMysqlTimestampString(new Date()),
-      ],
-      (error, results, fields) => {
-        error ? reject(error) : resolve(results);
-      }
-    );
-  });
-}
-
-function createMeatParticipant(meatId, userId) {
-  return new Promise((resolve, reject) => {
-    connection.query(
-      `INSERT INTO MEAT_USER (
-        ID,
-        MEAT_ID,
-        USER_ID,
-        ROLE,
-        STATUS,
-        CREATED_DATE,
-        LAST_MODIFIED_DATE
-      ) VALUES (${createPlaceholderString(7)})`,
-      [
-        null,
-        meatId,
-        userId,
-        MeatUserRole.PARTICIPANT,
+        meatUserRole,
         MeatUserStatus.GOING,
         toMysqlTimestampString(new Date()),
         toMysqlTimestampString(new Date()),
@@ -75,6 +51,34 @@ function updateMeatUserStatus(meatId, userId, status = MeatUserStatus.GOING) {
       [status, toMysqlTimestampString(new Date()), meatId, userId],
       (error, results, fields) => {
         error ? reject(error) : resolve(results);
+      }
+    );
+  });
+}
+
+function findAllMeatUserByMeatId(meatId) {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      `SELECT * FROM MEAT_USER
+        WHERE MEAT_ID = ?
+        `,
+      [meatId],
+      (error, results, fields) => {
+        error
+          ? reject(error)
+          : resolve(
+              results.map((result) => {
+                new MeatUser(
+                  result.id,
+                  result.meat_id,
+                  result.user_id,
+                  result.role,
+                  result.status,
+                  result.created_date,
+                  result.last_modified_date
+                );
+              })
+            );
       }
     );
   });
@@ -109,5 +113,8 @@ function findOneMeatUser(meatId, userId) {
 }
 
 module.exports = {
-  createMeatOrganiser,
+  createMeatUser,
+  updateMeatUserStatus,
+  findAllMeatUserByMeatId,
+  findOneMeatUser,
 };
