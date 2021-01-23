b583c6a050ed4ed18dba88f58a336390d43a3110
JKQuah <kobekai1997@gmail.com>
2021-01-23 16:13:29 +0800

    Add post route

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ require('./config/mysql');
 const indexRouter = require('./routers/index.router');
 const userRouter = require('./routers/users.router');
 const meatRouter = require('./routers/meats.router');
+const postRouter = require('./routers/posts.router');
 
 const app = express();
 
@@ -11,5 +12,6 @@ app.use(express.json());
 app.use(indexRouter);
 app.use(userRouter);
 app.use(meatRouter);
+app.use(postRouter);
 
 module.exports = app;
