956c86108085651fe4adb7a61cc719fd0fe1aab2
Ka Chun <34387770+kachun333@users.noreply.github.com>
2021-01-26 00:13:17 +0800

    fix auto merge branch mistakes

--- src/routers/posts.router.js
+++ src/routers/posts.router.js
@@ -8,7 +8,6 @@ const { route } = require('./index.router');
 const { hasMissingKey } = require('../utils/compare.utils');
 const auth = require("../middleware/auth.middleware");
 const { createStorage } = require("../repository/storage.repository");
-const { createStorage } = require("../repository/storage.repostitory");
 const LocationDTO = require('../dtos/locationDTO.dto');
 const { createLocation } = require('../repository/location.repostitory');
 const router = express.Router();
